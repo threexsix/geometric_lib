07a7abbe245b5d3f27a556fddd768189778dee57
threexsix <leogri3x6@gmail.com>
2023-12-21 00:51:59 +0300

    feat: square value validation

--- square.py
+++ square.py
@@ -1,7 +1,11 @@
 
 def area(a):
+    if a < 0:
+        raise ValueError("value cannot be negative")
     return a * a
 
 
 def perimeter(a):
+    if a < 0:
+        raise ValueError("value cannot be negative")
     return 4 * a
