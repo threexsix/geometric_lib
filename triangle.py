aeb79933a01b0bb56e5bfb78ad74568927e2b5b5
threexsix <leogri3x6@gmail.com>
2023-12-21 00:52:47 +0300

    feat: triangle value validation

--- triangle.py
+++ triangle.py
@@ -1,5 +1,10 @@
 def area(a, h):
+    if a < 0 or h < 0:
+        raise ValueError("value cannot be negative")
     return a * h / 2
 
+
 def perimeter(a, b, c):
-    return a + b + c 
+    if a < 0 or b < 0 or c < 0:
+        raise ValueError("value cannot be negative")
+    return a + b + c
