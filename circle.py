a8a29414edb3948ed85d3fb5ff525a85016108c5
threexsix <leogri3x6@gmail.com>
2023-12-21 00:50:30 +0300

    feat: circle value validation

--- circle.py
+++ circle.py
@@ -2,9 +2,13 @@ import math
 
 
 def area(r):
+    if r < 0:
+        raise ValueError("value cannot be negative")
     return math.pi * r * r
 
 
 def perimeter(r):
+    if r < 0:
+        raise ValueError("value cannot be negative")
     return 2 * math.pi * r
 
