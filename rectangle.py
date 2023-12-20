2d6e80c54c0ae0561264e32a8aff6aa3fd838cd7
threexsix <leogri3x6@gmail.com>
2023-12-21 00:51:19 +0300

    feat: rectangle value validation

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,10 @@
 def area(a, b):
+    if a < 0 or b < 0:
+        raise ValueError("value cannot be negative")
     return a * b
 
+
 def perimeter(a, b):
-    return 2 * (a + b) 
+    if a < 0 or b < 0:
+        raise ValueError("value cannot be negative")
+    return 2 * (a + b)
