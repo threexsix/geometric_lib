36c1efdfc25276cd6a065d0306f3808bcdb40ad1
threexsix <leogri3x6@gmail.com>
2023-12-21 13:39:03 +0300

    fix: value validations removed

--- triangle.py
+++ triangle.py
@@ -1,10 +1,6 @@
 def area(a, h):
-    if a < 0 or h < 0:
-        raise ValueError("value cannot be negative")
     return a * h / 2
 
 
 def perimeter(a, b, c):
-    if a < 0 or b < 0 or c < 0:
-        raise ValueError("value cannot be negative")
     return a + b + c
