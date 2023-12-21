4cd1488582d2f3f9ebf87b24e205494a6ce26c84
threexsix <leogri3x6@gmail.com>
2023-12-21 13:39:03 +0300

    fix: value validations removed

--- circle.py
+++ circle.py
@@ -2,13 +2,9 @@ import math
 
 
 def area(r):
-    if r < 0:
-        raise ValueError("value cannot be negative")
     return math.pi * r * r
 
 
 def perimeter(r):
-    if r < 0:
-        raise ValueError("value cannot be negative")
     return 2 * math.pi * r
 
--- rectangle.py
+++ rectangle.py
@@ -1,10 +1,6 @@
 def area(a, b):
-    if a < 0 or b < 0:
-        raise ValueError("value cannot be negative")
     return a * b
 
 
 def perimeter(a, b):
-    if a < 0 or b < 0:
-        raise ValueError("value cannot be negative")
     return 2 * (a + b)
